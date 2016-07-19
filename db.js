67709136ddd7202e3629cd1efb8463751ed8e047
tony-luisi <t.luisi@gmail.com>
2016-07-19 16:04:33 +1200

    added firebase

--- db.js
+++ db.js
@@ -1,1 +1,13 @@
 var firebase = require('firebase')
+
+firebase.initializeApp({
+  serviceAccount: './test-b3de52be5397.json',
+  databaseURL: 'https://test-ebd6b.firebaseio.com/'
+})
+
+var data = require('./data.json')
+
+var db = firebase.database()
+var ref = db.ref('server')
+
+var usersRef = ref.child('users')
